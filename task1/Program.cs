3d61cefbe24bd5f454d1a8339b1ab12434648732
Aleksander <kewgine@gmail.com>
2022-08-22 17:43:27 +0300

    task1&task2

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number1 == number2) 
+{
+Console.WriteLine("Числа равны");
+}
+else
+{
+    if (number1 > number2)
+    {
+        Console.WriteLine("max = " + max);
+    }
+    else
+    {
+        max=number2;
+        Console.WriteLine("max = " + max);
+    }
+}
