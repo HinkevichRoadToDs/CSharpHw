05a5fd319e922f21d2d7b1fb89a8c3fa6819f37a
Aleksander <kewgine@gmail.com>
2022-08-22 18:23:28 +0300

    add homework1-branch

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ( number % 2 == 1) 
+{
+    Console.Write("Число " + number + " является нечетным");
+}
+else
+{
+    Console.Write("Число " + number + " является четным");
+}
+
